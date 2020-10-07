ba499a10a2d9bfbdd4813ff310e752bdb76ee23e
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-10-07 12:19:03 +0200

    Created boilerplate components & basic app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Cart from './components/Cart';
+import AddItems from './components/AddItems';
 
 const App: React.FC = () => {
   return (
     <div>
       <Navbar />
       <main className="container mt-5">
-        <h1>App</h1>
+        <div className="row">
+          <div className="col">
+            <Cart />
+          </div>
+          <div className="col">
+            <AddItems />
+          </div>
+        </div>
       </main>
     </div>
   );
