c71dc051518d3feeb342be13f86d384f7a19ae43
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-10-07 14:36:40 +0200

    Implemented cart view & functionlity to add & remove items

--- src/components/AddItems.tsx
+++ src/components/AddItems.tsx
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Product from '../models/Product';
 
-const AddItems: React.FC = () => {
+const AddItems: React.FC<AddItemsProps> = ({ addItem }) => {
+  const [products, setProducts] = useState([
+    { name: 'apple', price: 4 },
+    { name: 'banana', price: 6.2 },
+    { name: 'strawberry', price: 2 },
+    { name: 'banana', price: 6.2 },
+    { name: 'banana', price: 6.2 },
+    { name: 'banana', price: 6.2 },
+    { name: 'banana', price: 6.2 },
+    { name: 'banana', price: 6.2 },
+    { name: 'banana', price: 6.2 },
+    { name: 'banana', price: 6.2 },
+  ]);
+
   return (
     <div>
-      <h2>Add Items</h2>
+      <h2>Add Items To Cart</h2>
+      <br />
+      <form action="POST" onSubmit={(e) => e.preventDefault()}>
+        <div className="form-group">
+          <textarea
+            name="items"
+            cols={30}
+            rows={5}
+            className="form-control"
+          ></textarea>
+        </div>
+      </form>
+      <br />
+      <div className="row">
+        {products.map((product, index) => (
+          <div key={index} className="col-3 mt-2">
+            <button
+              className="btn btn-primary"
+              onClick={() =>
+                addItem({ name: product.name, price: product.price })
+              }
+            >
+              {product.name} ({product.price.toFixed(2)})
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
+
+interface AddItemsProps {
+  addItem: (product: Product) => void;
+}
 
 export default AddItems;
--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,11 +1,66 @@
 import React from 'react';
+import CardItem from '../models/CartItem';
 
-const Cart: React.FC = () => {
+const Cart: React.FC<CartProps> = ({ cart, deleteItem }) => {
   return (
     <div>
-      <h2>Cart</h2>
+      <h2>Shopping Cart</h2>
+      <br />
+      {cart && cart.length > 0 ? (
+        <div className="row">
+          <table className="table">
+            <thead className="thead-dark">
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Quantity</th>
+                <th scope="col">Unit Price</th>
+                <th scope="col">Total Price</th>
+                <th scope="col">Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {cart.map((item) => (
+                <tr key={item.name}>
+                  <td>{item.name}</td>
+                  <td>{item.quantity}</td>
+                  <td>{item.unitPrice.toFixed(2)}</td>
+                  <td>{item.totalPrice.toFixed(2)}</td>
+                  <td>
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => deleteItem(item.name)}
+                    >
+                      Delete Item
+                    </button>
+                  </td>
+                </tr>
+              ))}
+              <tr>
+                <th className="bg-primary"></th>
+                <th className="bg-primary">
+                  {cart.reduce((acc, item) => acc + +item.quantity, 0)}
+                </th>
+                <th className="bg-primary"></th>
+                <th className="bg-primary">
+                  {cart
+                    .reduce((acc, item) => acc + +item.totalPrice, 0)
+                    .toFixed(2)}
+                </th>
+                <th className="bg-primary"></th>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <h5 className="text-center pt-5">No items in card yet</h5>
+      )}
     </div>
   );
 };
+
+interface CartProps {
+  deleteItem: (id: String) => void;
+  cart: Array<CardItem>;
+}
 
 export default Cart;
