d53e0527e2852b57532181f21e70ca95b41ac38c
Patrick Obermeier <patrick.obermeier@outlook.com>
2020-10-07 14:34:52 +0200

    Added app state & cart functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import Cart from './components/Cart';
 import AddItems from './components/AddItems';
+import Product from './models/Product';
+import CardItem from './models/CartItem';
 
-const App: React.FC = () => {
+const App: React.FC<{ initial?: Array<CardItem> }> = ({ initial = [] }) => {
+  const [cart, setCart] = useState(initial);
+
+  const addItem = (product: Product) => {
+    const { name, price } = product;
+
+    const found: any = cart.find((item: CardItem) => item.name === name);
+
+    if (found) {
+      found.quantity = +found.quantity + 1;
+      found.totalPrice = +found.quantity * +found.unitPrice;
+
+      setCart((prevCart) =>
+        prevCart.map((item) => (item.name === found.name ? found : item)),
+      );
+    } else {
+      const newItem: CardItem = {
+        name,
+        unitPrice: price,
+        totalPrice: price,
+        quantity: 1,
+      };
+
+      setCart((prevCart) => [...prevCart, newItem]);
+    }
+  };
+
+  const deleteItem = (name: String) => {
+    setCart((prevCart) => prevCart.filter((item) => item.name !== name));
+  };
+
   return (
     <div>
       <Navbar />
       <main className="container mt-5">
         <div className="row">
-          <div className="col">
-            <Cart />
+          <div className="col-7 pr-5">
+            <Cart cart={cart} deleteItem={deleteItem} />
           </div>
-          <div className="col">
-            <AddItems />
+          <div className="col-5 pl-5">
+            <AddItems addItem={addItem} />
           </div>
         </div>
       </main>
